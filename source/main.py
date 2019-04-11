36f8b945c4ad81a4e6579124bd4f1f50915e7869
Jose Torronteras <josetorronteras95@gmail.com>
2019-04-11 11:22:23 +0200

    Archivos con los modelos y cambio en el main

--- source/main.py
+++ source/main.py
@@ -1,10 +1,20 @@
 import configparser
 import argparse
 import matplotlib.pyplot as plt
-
 import os
 import json
-os.environ["CUDA_VISIBLE_DEVICES"]="1";
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--preprocess", "-p", help = "Preparar los datos de las canciones", action = "store_true")
+parser.add_argument("--dataset", "-d", help = "Preparar los datos para el entrenamiento", action = "store_true")
+parser.add_argument("--trainmodel", "-t", help = "Entrenar el modelo", action = "store_true")
+parser.add_argument("--model", "-m ", help = "Archivo con los parámetros del modelo", action = "store_true")
+parser.add_argument("--config", "-c", help = "Archivo de Configuracion")
+parser.add_argument("--device", "-d", help = "Cuda Visible Device")
+args = parser.parse_args()
+
+# Seleccionamos la gpu disponible
+os.environ["CUDA_VISIBLE_DEVICES"] = int(args.device);
 
 from keras import optimizers
 from keras import losses
@@ -16,13 +26,6 @@ from os.path import isfile
 from Extract_Audio_Features import ExtractAudioFeatures
 from Get_Train_Test_Data import GetTrainTestData
 from Create_Model import CNNModel
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--preprocess", "-p", help = "Preparar los datos de las canciones", action = "store_true")
-parser.add_argument("--dataset", "-d", help = "Preparar los datos para el entrenamiento", action = "store_true")
-parser.add_argument("--trainmodel", "-t", help = "Entrenar el modelo", action = "store_true")
-parser.add_argument("--config", "-c", help = "Archivo de Configuracion")
-args = parser.parse_args()
 
 config = configparser.ConfigParser()
 config.read(args.config)
@@ -43,98 +46,91 @@ elif args.trainmodel:
     y_test = np_utils.to_categorical(y_test)
     y_val = np_utils.to_categorical(y_val)
 
-    with open('data/models.json') as json_data:
-        modelos = json.load(json_data)
+    with open(args.model) as json_data:
+        modelo = json.load(json_data)
 
-    for i in range(len(modelos)):
+    try:
+        os.mkdir(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelo['id']))
+    except:
+        print("No se ha podido crear la carpeta")
+        pass
 
-        try:
-            os.mkdir(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelos[list(modelos.keys())[i]['id']]))
-            os.remove('/home/jjtorronteras/Music-Genre-Classification/checkpoint.hdf5')
-        except:
-            print("No se ha podido crear la carpeta")
-            pass
+    # Creamos el modelo
+    model = CNNModel(config, modelo, X_train).build_model(nb_classes = y_test.shape[1])
 
-        try:
-            # Creamos el modelo
-            model = CNNModel(config, modelos[list(modelos.keys())[i]], X_train).build_model(nb_classes = y_test.shape[1])
-
-            model.compile(loss = losses.categorical_crossentropy,
-                        #optimizer = optimizers.Adam(lr = 0.001),
-                        optimizer = optimizers.SGD(lr = 0.001, momentum = 0, decay = 1e-5, nesterov = True),
-                        metrics = ['accuracy'])
-            model.summary()
+    model.compile(loss = losses.categorical_crossentropy,
+                #optimizer = optimizers.Adam(lr = 0.001),
+                optimizer = optimizers.SGD(lr = 0.001, momentum = 0, decay = 1e-5, nesterov = True),
+                metrics = ['accuracy'])
+    model.summary()
             
-            # Guardamos el Modelo
-            model_json = model.to_json()
-            with open(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelos[list(modelos.keys())[i]]['id']) + "/model.json", "w") as json_file:
-                json_file.write(model_json)
+    # Guardamos el Modelo
+    model_json = model.to_json()
+    with open(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelo['id']) + "/model.json", "w") as json_file:
+        json_file.write(model_json)
 
-            # Comprobamos si hay un fichero checkpoint
-            if int(config['CALLBACKS']['LOAD_CHECKPOINT']):
-                print("Buscando fichero Checkpoint...")
-                if isfile(config['CALLBACKS']['CHECKPOINT_FILE']):
-                    print('Fichero Checkpoint detectando. Cargando weights.')
-                    model.load_weights(config['CALLBACKS']['CHECKPOINT_FILE'])
-                else:
-                    print('No se ha detectado el fichero Checkpoint.  Empezando de cero')
-            else:
-                print('No Checkpoint')
+    # Comprobamos si hay un fichero checkpoint
+    if int(config['CALLBACKS']['LOAD_CHECKPOINT']):
+        print("Buscando fichero Checkpoint...")
+        if isfile(config['CALLBACKS']['CHECKPOINT_FILE']):
+            print('Fichero Checkpoint detectando. Cargando weights.')
+            model.load_weights(config['CALLBACKS']['CHECKPOINT_FILE'])
+        else:
+            print('No se ha detectado el fichero Checkpoint.  Empezando de cero')
+    else:
+        print('No Checkpoint')
             
-            # Creamos los Callbacks
-            """
-                                    ModelCheckpoint(filepath = config['CALLBACKS']['CHECKPOINT_FILE'],
-                                        verbose = 1,
-                                        save_best_only = True,
-                                    ),"""
-            callbacks = [
-                        TensorBoard(log_dir = config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelos[list(modelos.keys())[i]]['id']),
-                                    write_images = config['CALLBACKS']['TENSORBOARD_WRITEIMAGES'],
-                                    write_graph = config['CALLBACKS']['TENSORBOARD_WRITEGRAPH'],
-                                    update_freq = config['CALLBACKS']['TENSORBOARD_UPDATEFREQ']
-                                    ),
-                        EarlyStopping(monitor = config['CALLBACKS']['EARLYSTOPPING_MONITOR'],
-                                    mode = config['CALLBACKS']['EARLYSTOPPING_MODE'], 
-                                    patience = int(config['CALLBACKS']['EARLYSTOPPING_PATIENCE']),
-                                    verbose = 1)
-            ]
+    # Creamos los Callbacks
+    """
+    ModelCheckpoint(filepath = config['CALLBACKS']['CHECKPOINT_FILE'],
+        verbose = 1,
+        save_best_only = True,
+    ),
+    """
+    callbacks = [
+                TensorBoard(log_dir = config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelo['id']),
+                            write_images = config['CALLBACKS']['TENSORBOARD_WRITEIMAGES'],
+                            write_graph = config['CALLBACKS']['TENSORBOARD_WRITEGRAPH'],
+                            update_freq = config['CALLBACKS']['TENSORBOARD_UPDATEFREQ']
+                            ),
+                EarlyStopping(monitor = config['CALLBACKS']['EARLYSTOPPING_MONITOR'],
+                            mode = config['CALLBACKS']['EARLYSTOPPING_MODE'], 
+                            patience = int(config['CALLBACKS']['EARLYSTOPPING_PATIENCE']),
+                            verbose = 1)
+    ]
 
-            # Entrenamos el modelo
-            history = model.fit(
-                                X_train,
-                                y_train,
-                                batch_size = int(config['CNN_CONFIGURATION']['BATCH_SIZE']),
-                                epochs = int(config['CNN_CONFIGURATION']['NUMBERS_EPOCH']),
-                                verbose = 1,
-                                validation_data = (X_val, y_val),
-                                callbacks = callbacks)
+    # Entrenamos el modelo
+    history = model.fit(
+                        X_train,
+                        y_train,
+                        batch_size = int(config['CNN_CONFIGURATION']['BATCH_SIZE']),
+                        epochs = int(config['CNN_CONFIGURATION']['NUMBERS_EPOCH']),
+                        verbose = 1,
+                        validation_data = (X_val, y_val),
+                        callbacks = callbacks)
 
-            score = model.evaluate(X_test, y_test, verbose=0)
-            print('Test score:', score[0])
-            print('Test accuracy:', score[1])
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
 
-            print(history.history.keys())
+    print(history.history.keys())
 
-            # summarize history for accuracy
-            plt.plot(history.history['acc'])
-            plt.plot(history.history['val_acc'])
-            plt.title('model accuracy')
-            plt.ylabel('accuracy')
-            plt.xlabel('epoch')
-            plt.legend(['train', 'test'], loc='upper left')
-            plt.savefig(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelos[list(modelos.keys())[i]]['id']) +  '/acc.png')
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelo['id']) +  '/acc.png')
 
-            # summarize history for loss
-            plt.plot(history.history['loss'])
-            plt.plot(history.history['val_loss'])
-            plt.title('model loss')
-            plt.ylabel('loss')
-            plt.xlabel('epoch')
-            plt.legend(['train', 'test'], loc='upper left')
-            plt.savefig(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelos[list(modelos.keys())[i]]['id']) + '/loss.png')
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(config['CALLBACKS']['TENSORBOARD_LOGDIR'] + str(modelo['id']) + '/loss.png')
 
-            model.save_weights(config['PATH_CONFIGURATION']['OUTPUT'] + config['OUTPUT']['WEIGHTS_FILE'])
-        except ValueError:
-            print("No se ha podido crear o entrenar")
-            print(modelos[list(modelos.keys())[i]]['id'])
-            pass
+    model.save_weights(config['PATH_CONFIGURATION']['OUTPUT'] + config['OUTPUT']['WEIGHTS_FILE'])
