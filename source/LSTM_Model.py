ad5e4ea02f4ec5989d66d0c3f135aef7f6aa4465
Jose Torronteras <josetorronteras95@gmail.com>
2019-05-07 22:27:27 +0200

    [New Model] Probando un nuevo modelo

--- source/LSTM_Model.py
+++ source/LSTM_Model.py
@@ -6,7 +6,7 @@ from keras.models import Sequential
 from keras.layers.recurrent import LSTM
 from keras.layers import Dense
 from keras import optimizers
-
+from keras import losses
 
 class LSTMModel():
     """
@@ -68,7 +68,10 @@ class LSTMModel():
         self.model.add(LSTM(units=64, dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
         self.model.add(Dense(units=10, activation='softmax'))
 
-        self.model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.001), metrics=['accuracy'])
+        self.model.compile(loss=losses.categorical_crossentropy',
+                        optimizer=optimizers.SGD(lr=0.01),
+                        metrics=['accuracy'])
+
         self.model.summary()
         
     def trainModel(self, config, *data, callbacks):
